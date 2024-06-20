13203d4f69c5af31bb6582af26c1bfb90400202f
cleissongomes <cleissongomes777@gmail.com>
2024-06-20 10:13:40 -0300

    feat(db): add PG library and create connection pool in db.js

---
+++ repositories/db.js
@@ -0,0 +1,18 @@
+import pg from 'pg';
+
+async function connect() {
+  if (global.connection) {
+    return global.connection.connect();
+  }
+
+  const pool = new pg.Pool({
+    connectionString:
+      'postgres://xdlezwnr:R7olnOB9UOIMoQDoYmBTeIeYXjHgIlvd@castor.db.elephantsql.com/xdlezwnr',
+  });
+  global.connection = pool;
+  return pool.connect();
+}
+
+export default {
+  connect,
+};
