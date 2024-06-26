a6b284c7173c1f529fedb355616f94dc0131169f
cleissongomes <cleissongomes777@gmail.com>
2024-06-26 09:38:00 -0300

    feat(clients): add updateClient function to controller, service, repository, and routes

--- controllers/client.controller.js
+++ controllers/client.controller.js
@@ -12,7 +12,8 @@ async function createClient(req, res, next) {
     ) {
       throw new Error('Name, CPF, Phone, Email e Address são obrigatórios.');
     }
-    res.send(await ClientService.createClient(client));
+    client = await ClientService.createClient(client);
+    res.send(client);
     logger.info(`POST /client - ${JSON.stringify(client)}`);
   } catch (err) {
     next(err);
@@ -47,9 +48,33 @@ async function deleteClient(req, res, next) {
   }
 }
 
+async function updateClient(req, res, next) {
+  try {
+    let client = req.body;
+    if (
+      !client.client_id ||
+      !client.name ||
+      !client.cpf ||
+      !client.phone ||
+      !client.email ||
+      !client.address
+    ) {
+      throw new Error(
+        'Client ID, Name, CPF, Phone, Email e Address são obrigatórios.'
+      );
+    }
+    client = await ClientService.updateClient(client);
+    res.send(client);
+    logger.info(`PUT /client - ${JSON.stringify(client)}`);
+  } catch (err) {
+    next(err);
+  }
+}
+
 export default {
   createClient,
   getClients,
   getClient,
   deleteClient,
+  updateClient,
 };
